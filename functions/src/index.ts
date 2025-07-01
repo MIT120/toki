31899bd8fa21e112c0276cf65b3a08baa63d83b8
Martin Savov <mataharata@gmail.com>
2025-07-01 13:51:35 +0300

    Removes Firebase Functions
    Removes all code related to Firebase Functions from the project.
    This functionality is no longer required.

--- functions/src/index.ts
+++
@@ -1,9 +0,0 @@
-import * as functions from "firebase-functions";
-
-export const prices = functions.https.onRequest((_, response) => {
-    const res: number[] = []
-    for (let i = 0; i < 24; i++) {
-        res.push(Math.floor(Math.random() * 600 * 100) / 100 + 50)
-    }
-    response.send(res);
-});
