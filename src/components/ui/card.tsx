52df94672da58a9ec161931867af816508912039
Martin Savov <mataharata@gmail.com>
2025-07-01 15:03:01 +0300

    Fix build and merge tailwind

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,6 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
 import * as React from "react"
+import { twMerge } from "tailwind-merge"
 
-import { cn } from "../../lib/utils"
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const Card = React.forwardRef<
   HTMLDivElement,
