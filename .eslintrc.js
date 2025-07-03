ca24c668148c92441dfc4f49efda2c417018196b
Martin Savov <mataharata@gmail.com>
2025-07-03 13:30:29 +0300

    change to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,27 @@
- 
+module.exports = {
+    extends: ['next/core-web-vitals'],
+    root: true,
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        node: true,
+        es2022: true,
+    },
+    rules: {
+        // Add any custom rules here
+        '@next/next/no-html-link-for-pages': 'error',
+        'react/no-unescaped-entities': 'off',
+        '@next/next/no-page-custom-font': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+}; 
